22a7fec8e18ac1ac9232159946c63d5fa5e9fdb2
Itoshirino <farruxzet@gmail.com>
2025-12-19 19:33:10 +0300

    beta

--- Utills/login.js
+++ Utills/login.js
@@ -1,61 +1,129 @@
 const elForm = document.querySelector(".form");
-const api = `https://fakestoreapi.com/auth/login`;
-const btn = document.querySelector(".form__btn");
+const api = "https://fakestoreapi.com/auth/login";
+const adminBtn = document.querySelector(".form__btn");
+const userBtn = document.querySelector(".form__btn2");
 const loginBtn = document.querySelector(".btn");
-document.querySelector(".password__icon").addEventListener("click", () => {
-  const passwd = document.querySelector(".passwd");
-  const icon = document.querySelector(".passwd__icon");
+const securityForm = document.querySelector(".security");
+const securityBtn = document.querySelector(".security__btn");
+const exitBtn = document.querySelector(".security__btn2");
+const securityInput = document.querySelector(".security__input");
+const userInput = document.querySelector(".username");
+const passwd = document.querySelector(".passwd");
+const passwordIcon = document.querySelector(".passwd__icon");
+
+exitBtn.addEventListener("click", () => {
+  securityForm.style.display = "none";
+  elForm.style.display = "block";
+});
+
+passwordIcon.addEventListener("click", (e) => {
+  e.preventDefault();
+
   if (passwd.type === "password") {
     passwd.type = "text";
-    icon.classList.remove("ri-eye-line");
-    icon.classList.add("ri-eye-off-line");
+    passwordIcon.classList.replace("ri-eye-line", "ri-eye-off-line");
   } else {
     passwd.type = "password";
-    icon.classList.remove("ri-eye-off-line");
-    icon.classList.add("ri-eye-line");
+    passwordIcon.classList.replace("ri-eye-off-line", "ri-eye-line");
   }
 });
 
-btn.addEventListener("click", () => {
-  Toastify({
-    text: `Username: derek 
-     Password: jklg*_56`,
-    duration: 7000,
-    style: {
-      background: "linear-gradient(to right, #b00000ff, red)",
-    },
-  }).showToast();
+securityBtn.addEventListener("click", () => {
+  if (securityInput.value.trim() === "admin") {
+    Toastify({
+      text: "Success! Please wait...",
+      duration: 3000,
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+    }).showToast();
+    setTimeout(() => {
+      securityForm.style.display = "none";
+      elForm.style.display = "block";
+
+      userInput.value = "derek";
+      passwd.value = "jklg*_56";
+      securityInput.value = "";
+    }, 3000);
+  } else {
+    Toastify({
+      text: "Wrong Security Key!",
+      duration: 3000,
+      style: {
+        background: "linear-gradient(to right, #b00000, red)",
+      },
+    }).showToast();
+
+    securityInput.value = "";
+  }
 });
 
-loginBtn.addEventListener("click", () => {
-  if (elForm["username"].value === "" || elForm["password"].value === "") {
+adminBtn.addEventListener("click", () => {
+  securityForm.style.display = "block";
+  elForm.style.display = "none";
+});
+
+userBtn.addEventListener("click", () => {
+  Toastify({
+    text: "For User Login",
+    duration: 3000,
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+  }).showToast();
+
+  userInput.value = "user";
+  passwd.value = "user123";
+});
+
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = userInput.value.trim();
+  const password = passwd.value.trim();
+  if (!username || !password) {
     Toastify({
-      text: `Please fill the inputs`,
-      duration: 7000,
-      style: {
-        background: "linear-gradient(to right, #b00000ff, red)",
-      },
+      text: "Please fill the inputs",
+      duration: 3000,
+      style: { background: "red" },
     }).showToast();
     return;
   }
+
+  if (username === "derek" && password === "jklg*_56") {
+    Toastify({
+      text: "Admin login success",
+      duration: 2000,
+      style: { background: "green" },
+    }).showToast();
+
+    setTimeout(() => {
+      window.location.href = "../Pages/dashboard.html";
+    }, 1500);
+    return;
+  }
+
+  if (username === "user" && password === "user123") {
+    Toastify({
+      text: "User login success",
+      duration: 2000,
+      style: { background: "green" },
+    }).showToast();
+
+    setTimeout(() => {
+      window.location.href = "./Pages/user.html";
+    }, 1500);
+    return;
+  }
+
+  Toastify({
+    text: "Username or password is incorrect",
+    duration: 3000,
+    style: { background: "red" },
+  }).showToast();
 });
-const handleSubmit = (e) => {
-  e.preventDefault();
 
-  const username = elForm["username"].value.trim();
-  const password = elForm["password"].value.trim();
-  const user = {
-    username,
-    password,
-  };
-
-  axios.post(api, user).then((data) => {
-    const token = data.data.token;
-
-    localStorage.setItem("token", token);
-
-    window.location.href = "../Pages/dashboard.html";
-  });
-};
-
-elForm.addEventListener("submit", handleSubmit);
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+  }
+});
